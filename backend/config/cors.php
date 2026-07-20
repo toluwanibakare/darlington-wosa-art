cec0e88d3baf79c316060448bc00eb17f6cb86a5
TMB <146067045+toluwanibakare@users.noreply.github.com>
2026-07-20 11:28:01 +0100

    add ngrok URL to CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'https://darlington-wosa-art.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'https://darlington-wosa-art.vercel.app', 'https://dyslexic-pelvis-sequester.ngrok-free.dev'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
